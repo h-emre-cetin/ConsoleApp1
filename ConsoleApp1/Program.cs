349bf423126b9768df0cc9ff91d53b0851da7093
EMRE <EMRE@DESKTOP-D3VV2GT>
2023-09-12 21:53:45 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.Metadata;
+
+Console.WriteLine("Hello, World!");
+
+int i = 3;
+object o = i;
+Console.WriteLine(o);
+
